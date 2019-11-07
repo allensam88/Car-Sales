1541bd2e70a0ce9e34436f62a4a1bf5611944fee
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-07 08:59:05 -0800

    cleaned up my code a bit

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
 const AddedFeature = props => {
-    console.log('component feature props:', props.feature);
-
     return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
       <button className="button" onClick={() => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
